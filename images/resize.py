ab12d3f67580f062b738d4670f48a4cd9ae0c75e
Arihant Parsoya <parsoyaarihant@gmail.com>
2019-11-11 11:41:43 +0530

    updated images

--- images/resize.py
+++ images/resize.py
@@ -9,14 +9,17 @@ basewidth = 600
 
 def resize():
 	for item in dirs:
-		if ('.png' in item) or ('.jpg' in item) or ('.PNG' in item):
-			print(item)
+		if ('.png' in item) or ('.jpg' in item) or ('.PNG' in item) or ('.JPG' in item):
 			img = Image.open(item)
 
 			wpercent = (basewidth/float(img.size[0]))
 			hsize = int((float(img.size[1])*float(wpercent)))
 
+			item = item.replace(".PNG", ".png")
+			item = item.replace(".JPG", ".png")
+
+			print(item)
 			imResize = img.resize((basewidth,hsize), Image.ANTIALIAS)
-			imResize.save("Resized/" + item, 'PNG', quality=90)
+			imResize.save("Resized/" + item, 'png', quality=90)
 
 resize()
